45a896c6242c33afec5ccf981437a24ff8a64a8b
Brian Rak <brak@indiana.edu>
2014-04-10 16:36:13 -0400

    fixed indentation

--- indoor_stm/main.c
+++ indoor_stm/main.c
@@ -52,76 +52,76 @@ f3d_lcd_drawPixel(x+ox,y+oy,color);
 */
 
 int main() { 
-				f3d_delay_init();
-				f3d_delay_uS(10);
-				f3d_uart_init();
-				f3d_delay_uS(10);
-				f3d_led_init();
-				f3d_delay_uS(10);
-				f3d_lcd_init();
-				f3d_delay_uS(10);
-				f3d_systick_init();
-				f3d_delay_uS(10);
-				f3d_i2c1_init();
-				f3d_delay_uS(10);
-				ds_nordic_init();
-				f3d_delay_uS(10);
+    f3d_delay_init();
+    f3d_delay_uS(10);
+    f3d_uart_init();
+    f3d_delay_uS(10);
+    f3d_led_init();
+    f3d_delay_uS(10);
+    f3d_lcd_init();
+    f3d_delay_uS(10);
+    f3d_systick_init();
+    f3d_delay_uS(10);
+    f3d_i2c1_init();
+    f3d_delay_uS(10);
+    ds_nordic_init();
+    f3d_delay_uS(10);
 
-				/*
+    /*
 
-					 f3d_lcd_fillScreen(WHITE);
-					 f3d_lcd_drawChar(20,80,'X',RED,WHITE);
-					 f3d_lcd_drawChar(60,80,'Y',GREEN,WHITE);
-					 f3d_lcd_drawChar(100,80,'Z',BLUE,WHITE);
-					 f3d_lcd_drawString(50,10,"GYRO",BLACK,WHITE);
+       f3d_lcd_fillScreen(WHITE);
+       f3d_lcd_drawChar(20,80,'X',RED,WHITE);
+       f3d_lcd_drawChar(60,80,'Y',GREEN,WHITE);
+       f3d_lcd_drawChar(100,80,'Z',BLUE,WHITE);
+       f3d_lcd_drawString(50,10,"GYRO",BLACK,WHITE);
 
-					 int k=0;
-					 */
-				nrf24l01_initialize_debug(true, 1, false);  // setup wifibase as a receiver, Standard Shockburst
-				nrf24l01_initialize_debug(true, 32, true);       // Enhanced Shockburst, Auto Ack turned on
-				nrf24l01_clear_flush();
-				printf("Begin wifi test\n");
-				// Base Reception
-				char rxdata[32];
-				char index;
-				while(!(nrf24l01_irq_pin_active() && nrf24l01_irq_rx_dr_active()));
-				nrf24l01_read_rx_payload(rxdata, 32);
-				nrf24l01_irq_clear_all();
-				f3d_delay_uS(130);
+       int k=0;
+       */
+    nrf24l01_initialize_debug(true, 1, false);  // setup wifibase as a receiver, Standard Shockburst
+    nrf24l01_initialize_debug(true, 32, true);       // Enhanced Shockburst, Auto Ack turned on
+    nrf24l01_clear_flush();
+    printf("Begin wifi test\n");
+    // Base Reception
+    char rxdata[32];
+    char index;
+    while(!(nrf24l01_irq_pin_active() && nrf24l01_irq_rx_dr_active()));
+    nrf24l01_read_rx_payload(rxdata, 32);
+    nrf24l01_irq_clear_all();
+    f3d_delay_uS(130);
 
-				// Payload Translation
-				printf("Base Data: ");
-				for (index=0;index<32;index++) {
-								printf("%c",rxdata[index]);
-				}
-				printf("\n");
+    // Payload Translation
+    printf("Base Data: ");
+    for (index=0;index<32;index++) {
+        printf("%c",rxdata[index]);
+    }
+    printf("\n");
 
-				for (index=0;index<32;index++) {
-								if ((rxdata[index] >= 'a') && (rxdata[index] <= 'z')) {
-												rxdata[index]-=32;
-								}
-				}
+    for (index=0;index<32;index++) {
+        if ((rxdata[index] >= 'a') && (rxdata[index] <= 'z')) {
+            rxdata[index]-=32;
+        }
+    }
 
-				printf("Base: receive character: %c\n",rxdata);
-				// rxdata-=32; 
-				printf("Base: upcase: %c\n",rxdata);
-				
+    printf("Base: receive character: %c\n",rxdata);
+    // rxdata-=32; 
+    printf("Base: upcase: %c\n",rxdata);
 
-				while(1){
-								f3d_led_all_on();
-								printf("a\n");
-								f3d_lcd_fillScreen(BLUE);
-								f3d_lcd_fillScreen(RED);
-								f3d_lcd_fillScreen(GREEN);
-				}
+
+    while(1){
+        f3d_led_all_on();
+        printf("a\n");
+        f3d_lcd_fillScreen(BLUE);
+        f3d_lcd_fillScreen(RED);
+        f3d_lcd_fillScreen(GREEN);
+    }
 
 }
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line) {
-				/* Infinite loop */
-				/* Use GDB to find out why we're here */
-				while (1);
+    /* Infinite loop */
+    /* Use GDB to find out why we're here */
+    while (1);
 }
 #endif
 
--- outside_stm/main.c
+++ outside_stm/main.c
@@ -14,97 +14,97 @@
 
 int main(){
 
-				setvbuf(stdin, NULL, _IONBF, 0);
-				setvbuf(stdout, NULL, _IONBF, 0);
-				setvbuf(stderr, NULL, _IONBF, 0);
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
 
-				f3d_delay_init();
-				f3d_delay_uS(10);
-				f3d_led_init();
-				f3d_delay_uS(10);
-				f3d_uart_init();
-				f3d_delay_uS(10);
-				f3d_systick_init();
-				f3d_delay_uS(10);
-				f3d_i2c1_init();
-				f3d_delay_uS(10);
-				RTC_init();
-				f3d_delay_uS(10);
-				f3d_timer2_init();
-				f3d_delay_uS(10);
-				f3d_dac_init();
-				f3d_delay_uS(10);
-				f3d_a2d_init();
-				f3d_delay_uS(10);
-				ds_wifibase_init();
-				f3d_delay_uS(10);
+    f3d_delay_init();
+    f3d_delay_uS(10);
+    f3d_led_init();
+    f3d_delay_uS(10);
+    f3d_uart_init();
+    f3d_delay_uS(10);
+    f3d_systick_init();
+    f3d_delay_uS(10);
+    f3d_i2c1_init();
+    f3d_delay_uS(10);
+    RTC_init();
+    f3d_delay_uS(10);
+    f3d_timer2_init();
+    f3d_delay_uS(10);
+    f3d_dac_init();
+    f3d_delay_uS(10);
+    f3d_a2d_init();
+    f3d_delay_uS(10);
+    ds_wifibase_init();
+    f3d_delay_uS(10);
 
-				/* nrf24l01base_initialize_debug(false, 1, false);  // Setup Node at transmitter, Standard Shockburst */
-				nrf24l01base_initialize_debug(false, 32, true);     // Enhanced Shockburst, Auto Ack turned on
-				nrf24l01base_clear_flush();
+    /* nrf24l01base_initialize_debug(false, 1, false);  // Setup Node at transmitter, Standard Shockburst */
+    nrf24l01base_initialize_debug(false, 32, true);     // Enhanced Shockburst, Auto Ack turned on
+    nrf24l01base_clear_flush();
 
-				char data[32];
-				char index;
+    char data[32];
+    char index;
 
-				for (index=0;index<32;index++) {
-								data[index] = 'a'+index;
-				}
-				printf("Node Data: ");
-				for (index=0;index<32;index++) {
-								printf("%c",data[index]);
-				}
-				printf("\n");
-				printf("Node: transmit character %c\n",data);
-				nrf24l01base_write_tx_payload(data, 32, true);      // nordic writes a character
-				while(!(nrf24l01base_irq_pin_active() && (nrf24l01base_irq_tx_ds_active() || nrf24l01base_irq_max_rt_active()))); // wait until it is gone
+    for (index=0;index<32;index++) {
+        data[index] = 'a'+index;
+    }
+    printf("Node Data: ");
+    for (index=0;index<32;index++) {
+        printf("%c",data[index]);
+    }
+    printf("\n");
+    printf("Node: transmit character %c\n",data);
+    nrf24l01base_write_tx_payload(data, 32, true);      // nordic writes a character
+    while(!(nrf24l01base_irq_pin_active() && (nrf24l01base_irq_tx_ds_active() || nrf24l01base_irq_max_rt_active()))); // wait until it is gone
 
-				//Node Listen Mode if the maximum retry limit was not hit
-				if (!nrf24l01base_irq_max_rt_active()) {
-				  nrf24l01base_irq_clear_all();
-				  nrf24l01base_set_as_rx(true);
-				  printf("true\n");
-				}
-				else {
-				  nrf24l01base_flush_tx(); //get the unsent character out of the TX FIFO
-				  nrf24l01base_irq_clear_all(); //clear all interrupts
-				  printf("Node: Failed to send %c\n",data);
-				}
+    //Node Listen Mode if the maximum retry limit was not hit
+    if (!nrf24l01base_irq_max_rt_active()) {
+        nrf24l01base_irq_clear_all();
+        nrf24l01base_set_as_rx(true);
+        printf("true\n");
+    }
+    else {
+        nrf24l01base_flush_tx(); //get the unsent character out of the TX FIFO
+        nrf24l01base_irq_clear_all(); //clear all interrupts
+        printf("Node: Failed to send %c\n",data);
+    }
 
-				// Node reception
+    // Node reception
 
-				printf("node reception\n");
-				char noderx[32];
-				while(!(nrf24l01base_irq_pin_active() && nrf24l01base_irq_rx_dr_active()));
-				nrf24l01base_read_rx_payload(noderx, 32);
+    printf("node reception\n");
+    char noderx[32];
+    while(!(nrf24l01base_irq_pin_active() && nrf24l01base_irq_rx_dr_active()));
+    nrf24l01base_read_rx_payload(noderx, 32);
 
-				printf("Node Data: ");
-				for (index=0;index<32;index++) {
-								printf("%c",noderx[index]);
-				}
-				printf("\n");
+    printf("Node Data: ");
+    for (index=0;index<32;index++) {
+        printf("%c",noderx[index]);
+    }
+    printf("\n");
 
-				printf("Node: Receive character %c\n",noderx);
-				nrf24l01base_irq_clear_all();
-				f3d_delay_uS(130);
-				nrf24l01base_set_as_tx();
+    printf("Node: Receive character %c\n",noderx);
+    nrf24l01base_irq_clear_all();
+    f3d_delay_uS(130);
+    nrf24l01base_set_as_tx();
 
 
-				while(1){
-								//printf("%d\n",f3d_read_adc());
-								printf("a\n");
-								f3d_led_all_on();
-								f3d_delay_uS(100);
-								f3d_led_all_off();
-				}
+    while(1){
+        //printf("%d\n",f3d_read_adc());
+        printf("a\n");
+        f3d_led_all_on();
+        f3d_delay_uS(100);
+        f3d_led_all_off();
+    }
 
 
 }
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line) {
-				/* Infinite loop */
-				/* Use GDB to find out why we're here */
-				while (1);
+    /* Infinite loop */
+    /* Use GDB to find out why we're here */
+    while (1);
 }
 #endif
 
